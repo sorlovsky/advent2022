236a95466353aafb2897519e736530bd27a0fa2b
Simon Orlovsky <simon.orlovsky@gmail.com>
2022-12-29 10:14:19 -0800

    starting day4

--- src/advent2022/day3.clj
+++ src/advent2022/day3.clj
@@ -15,6 +15,7 @@
     (if (>= int-val-of-letter 97)
       (- int-val-of-letter 96)
       (- int-val-of-letter 38))))
+
 (defn calculate-intersection
   [lists]
   (let [first-half (into #{} (first lists))
@@ -22,13 +23,19 @@
     (clojure.set/intersection first-half second-half)))
 
 (map calculate-intersection (map split-in-half instructions))
-(map #(reduce + (map get-priority %)) (map calculate-intersection (map split-in-half instructions)))
-(reduce + (map #(reduce + (map get-priority %)) (map calculate-intersection (map split-in-half instructions))))
+
+(map #(reduce + (map get-priority %))
+     (map calculate-intersection
+          (map split-in-half instructions)))
+
+(reduce + (map #(reduce + (map get-priority %))
+               (map calculate-intersection
+                    (map split-in-half instructions))))
 
 ;; part 2
 (def instructions (clojure.string/split-lines
                     (slurp "resources/day3.txt")))
-(map get-priority)
-(reduce + (map #(reduce + (map get-priority %)) (map #(apply clojure.set/intersection %) (partition 3 (map #(->> (seq %)
-                                                                                                                 (into #{})
-                                                                                                                 ) instructions)))))
+(reduce + (map #(reduce + (map get-priority %))
+               (map #(apply clojure.set/intersection %)
+                    (partition 3 (map #(->> (seq %)
+                                            (into #{})) instructions)))))
--- src/advent2022/day4.clj
+++ src/advent2022/day4.clj
@@ -1,1 +1,5 @@
 (ns advent2022.day4)
+
+(def instructions (clojure.string/split-lines
+                    (slurp "resources/day4sample.txt")))
+
