0bf810092a7e539ad160ab74e5f3dec9c30850c4
Simon Orlovsky <simon.orlovsky@gmail.com>
2022-12-30 09:32:27 -0800

    day4 pt1

--- src/advent2022/day4.clj
+++ src/advent2022/day4.clj
@@ -1,5 +1,35 @@
-(ns advent2022.day4)
+(ns advent2022.day4
+  (:require
+    [clojure.set :as set]
+    [clojure.string :as string]))
 
-(def instructions (clojure.string/split-lines
-                    (slurp "resources/day4sample.txt")))
+(def instructions (->> (slurp "resources/day4.txt")
+                       (clojure.string/split-lines)
+                       (map #(clojure.string/split % #","))))
 
+(def so-pair ["2-9" "6-8"])
+(def so-range "2-4")
+(defn parse-int
+  [s]
+  (Integer/parseInt s))
+(defn parse-range
+  [range]
+  (map parse-int (string/split range #"-")))
+
+(defn inclusive-range
+  [start end]
+  (range start (inc end)))
+
+(defn empty-set-difference?
+  [s1 s2]
+  (= #{} (set/difference s1 s2)))
+(defn does-encompass?
+  [pair]
+  (let [first-range (into #{} (apply inclusive-range (parse-range (first pair))))
+        second-range (into #{} (apply inclusive-range (parse-range (second pair))))]
+    (or (empty-set-difference? first-range second-range)
+        (empty-set-difference? second-range first-range))
+    ))
+
+(prn instructions)
+(count (filter true? (map does-encompass? instructions)))
